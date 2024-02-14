95dd01b897b5f55598e75bfc46de0e5e0bd00fd4
Onlydaniela <137676404+Onlydaniela@users.noreply.github.com>
2024-02-14 17:25:19 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const okBtn = document.querySelector('#okBtn');
+
+okBtn.addEventListener('click',function () {
+    alert('Feliz dìa a mi mejor amigo(?')
+})
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function ( ) {
+    const randomX = parseInt(Math.random ()*100);
+    const randomY = parseInt(Math.random ()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
